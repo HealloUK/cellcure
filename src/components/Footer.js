6e48b356739848306202aa5d57ea2bd4dd8e6746
HealloUk <109734631+HealloUk@users.noreply.github.com>
2022-09-20 02:46:09 +0300

    Revert Rich Text Footer for now!

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,204 +1,49 @@
 import React from "react";
 
 const Footer = () => {
-
+  
   return (
     <>
-
-      <div class="site-footer__extra">
+    <div class="site-footer__extra">
         <div class="container">
           <div class="site-footer__extra-title">Disclaimer</div>
-          <p>The result of the treatment may vary depending on the type of disease, the patient's condition, the number of
-            sessions of the treatment and many other factors. Articles, images, patient feedbacks, testimonials and all other materials on the site
-            are for informational purposes only. Stem cells are not a guaranteed cure for every disease.
+          <p>The result of the treatment may vary depending on the type of disease, the patient's condition, the number of 
+              sessions of the treatment and many other factors. Articles, images, patient feedbacks, testimonials and all other materials on the site 
+              are for informational purposes only. Stem cells are not a guaranteed cure for every disease.
           </p>
         </div>
       </div>
+      
+      <footer className="site-footer">
+        
+        <div className="site-footer__bottom">
+          <div className="container">
+            
+            <p className="site-footer__copy">
+              &copy; Copyright 2022 - Edge Medica
+            </p>
+
+            <div className="site-footer__social">
+              
+              
+            <a target="blank" href="https://www.facebook.com/heallouk">
+                  <i className="fab fa-facebook-square"></i>
+            </a>
+              
+            <a target="blank" href="https://www.instagram.com/heallouk/">
+                  <i className="fab fa-instagram"></i>
+            </a>
+            
+            </div>
+
+          </div>
+        </div>
+
+      </footer>
+
 
       
-      <div className="site-footer">
-
-
-        <footer class="text-center text-lg-start">
-
-          <div class="container p-4 pb-0">
-
-            <section class="">
-
-              <div class="row">
-
-                <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
-                  <h6 class="text-uppercase mb-4 font-weight-bold">
-                    Edge Medica
-                  </h6>
-                  <p>
-                    Here you can use rows and columns to organize your footer
-                    content. Lorem ipsum dolor sit amet, consectetur adipisicing
-                    elit.
-                  </p>
-                </div>
-
-
-                <hr class="w-100 clearfix d-md-none" />
-
-
-                <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
-                  <h6 class="text-uppercase mb-4">Diseases</h6>
-                  <div className="footer-nav__list">
-                    <p>
-                      <a class="text-white">Stem Cell Therapy for Autism</a>
-                    </p>
-                    <p>
-                      <a class="text-white">Stem Cell Therapy for MS</a>
-                    </p>
-                    <p>
-                      <a class="text-white">Stem Cell Therapy for Spinal Cord Injury</a>
-                    </p>
-                  </div>
-                </div>
-
-
-                <hr class="w-100 clearfix d-md-none" />
-
-
-                <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
-                  <h6 class="text-uppercase mb-4 font-weight-bold">
-                    Useful links
-                  </h6>
-                  <div className="footer-nav__list">
-                    <p>
-                      <a class="text-white">lorem ipsum</a>
-                    </p>
-                    <p>
-                      <a class="text-white">lorem ipsum</a>
-                    </p>
-                    <p>
-                      <a class="text-white">lorem ipsum</a>
-                    </p>
-                    <p>
-                      <a class="text-white">lorem ipsum</a>
-                    </p>
-                  </div>
-                </div>
-
-
-                <hr class="w-100 clearfix d-md-none" />
-
-
-                <div class="col-md-2 col-lg-2 col-xl-3 mx-auto mt-3">
-                  <h6 class="text-uppercase mb-4">About Us</h6>
-                  <div className="footer-nav__list">
-                    <p>
-                      <a class="text-white">About Us</a>
-                    </p>
-                    <p>
-                      <a class="text-white">Blog</a>
-                    </p>
-                    <p>
-                      <a class="text-white">Privacy Policy</a>
-                    </p>
-                    <p>
-                      <a class="text-white">Terms and Conditions</a>
-                    </p>
-                  </div>
-                </div>
-
-              </div>
-
-            </section>
-
-
-
-          </div>
-
-        </footer>
-
-      </div>
-
-
-      <footer className="site-footer">
-
-        <div className="site-footer__bottom">
-          <div className="container">
-
-            <p className="site-footer__copy">
-              &copy; Copyright 2022 - Edge Medica
-            </p>
-
-            <div className="site-footer__social">
-
-
-              <a target="blank" href="https://www.facebook.com/heallouk">
-                <i className="fab fa-facebook-square"></i>
-              </a>
-
-              <a target="blank" href="https://www.instagram.com/heallouk/">
-                <i className="fab fa-instagram"></i>
-              </a>
-
-            </div>
-
-          </div>
-        </div>
-
-      </footer>
-
-
-
-
-
     </>
   );
-
-
-  return (
-    <>
-      <div class="site-footer__extra">
-        <div class="container">
-          <div class="site-footer__extra-title">Disclaimer</div>
-          <p>The result of the treatment may vary depending on the type of disease, the patient's condition, the number of
-            sessions of the treatment and many other factors. Articles, images, patient feedbacks, testimonials and all other materials on the site
-            are for informational purposes only. Stem cells are not a guaranteed cure for every disease.
-          </p>
-        </div>
-      </div>
-
-      <footer className="site-footer">
-
-        <div className="site-footer__bottom">
-          <div className="container">
-
-            <p className="site-footer__copy">
-              &copy; Copyright 2022 - Edge Medica
-            </p>
-
-            <div className="site-footer__social">
-
-
-              <a target="blank" href="https://www.facebook.com/heallouk">
-                <i className="fab fa-facebook-square"></i>
-              </a>
-
-              <a target="blank" href="https://www.instagram.com/heallouk/">
-                <i className="fab fa-instagram"></i>
-              </a>
-
-            </div>
-
-          </div>
-        </div>
-
-      </footer>
-
-
-
-    </>
-  );
-
-
-
-
-
-
 };
 export default Footer;
