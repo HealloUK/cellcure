fc23b9a3c1eeeda5c9a05329a6dcb87eb6d35912
HealloUk <109734631+HealloUk@users.noreply.github.com>
2022-10-24 20:14:19 +0300

    Fix

--- src/components/CourseList.js
+++ src/components/CourseList.js
@@ -106,7 +106,7 @@ const CourseList = () => {
             <ul class="disease__item-list">
               <li><span>Autism Treatment with Stem Cells</span></li>
               <li><span>Stem Cell Therapy for Spinal Cord Injuries</span></li>
-              <li><Link to="multiple-sclerosis-treatment-with-stem-cells">Multiple Sclerosis Treatment with Stem Cells</Link></li>
+              <li><Link to="/multiple-sclerosis-treatment-with-stem-cells">Multiple Sclerosis Treatment with Stem Cells</Link></li>
               <li><span>Stem Cell Therapy for Cerebral Palsy</span></li>
               <li><span>Alzheimer’s Disease Treatment with Stem Cells Therapy</span></li>
               <li><span>Stem Cell Therapy for Parkinson’s Disease</span></li>
